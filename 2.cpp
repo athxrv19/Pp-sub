20e5f2fb258dbbe9e94ab5c51be46ea64513ad83
Atharv Gaikwad <123192813+athxrv19@users.noreply.github.com>
2023-12-07 08:40:12 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,3 +1,133 @@
+//using inheriantance
+
+#include <iostream>
+
+class PowerCalculator {
+public:
+    virtual double calculatePower(double n, int p) = 0;
+};
+
+class RecursivePowerCalculator : public PowerCalculator {
+public:
+    double calculatePower(double n, int p) override {
+        if (p == 0) {
+            return 1.0;
+        } else if (p == 1) {
+            return n;
+        } else {
+            return n * calculatePower(n, p - 1);
+        }
+    }
+};
+
+class IterativePowerCalculator : public PowerCalculator {
+public:
+    double calculatePower(double n, int p) override {
+        double result = 1.0;
+        for (int i = 0; i < p; i++) {
+            result *= n;
+        }
+        return result;
+    }
+};
+
+int main() {
+    double n;
+    int p;
+
+    std::cout << "Enter the base value: ";
+    std::cin >> n;
+
+    std::cout << "Enter the power value: ";
+    std::cin >> p;
+
+    // Create an instance of RecursivePowerCalculator
+    RecursivePowerCalculator recursiveCalculator;
+    double recursiveResult = recursiveCalculator.calculatePower(n, p);
+
+    // Create an instance of IterativePowerCalculator
+    IterativePowerCalculator iterativeCalculator;
+    double iterativeResult = iterativeCalculator.calculatePower(n, p);
+
+    std::cout << "Recursive power: " << recursiveResult << std::endl;
+    std::cout << "Iterative power: " << iterativeResult << std::endl;
+
+    return 0;
+}
+
+//using exception handling
+#include <iostream>
+
+using namespace std;
+
+class PowerException : public exception {
+public:
+    PowerException(const string& message) : message(message) {}
+
+    const string& getMessage() const {
+        return message;
+    }
+
+private:
+    string message;
+};
+
+double power(double n, int p) {
+    if (n == 0 && p == 0) {
+        throw PowerException("Invalid input: n cannot be 0 when p is 0");
+    }
+
+    if (n < 0) {
+        throw PowerException("Invalid input: n cannot be negative");
+    }
+
+    if (p < 0) {
+        throw PowerException("Invalid input: p cannot be negative");
+    }
+
+    if (p == 0) {
+        return 1.0;
+    } else if (p == 1) {
+        return n;
+    } else {
+        return n * power(n, p - 1);
+    }
+}
+
+int main() {
+    double n;
+    int p;
+
+    cout << "Enter the base value: ";
+    cin >> n;
+
+    cout << "Enter the power value: ";
+    cin >> p;
+
+    try {
+        double result = power(n, p);
+        cout << "The power of " << n << " raised to " << p << " is: " << result << endl;
+    } catch (PowerException& e) {
+        cerr << "Error: " << e.getMessage() << endl;
+    }
+
+    return 0;
+}
+
+//using virtual function
+
+#include <iostream>
+
+class PowerCalculator {
+public:
+    virtual double calculatePower(double n, int p) = 0;
+};
+
+class RecursivePowerCalculator : public PowerCalculator {
+public:
+    double calculatePower(double n, int p) override {
+        if (p == 0) {
+            return 1.0;
         } else if (p == 1) {
             return n;
         } else {
